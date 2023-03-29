a5f35bf5a60adad4b56e8bd0df826a5c0bfaec24
pedrolauro15 <pedrolauro15@gmail.com>
2023-03-29 16:38:48 -0300

    TimeCounting

--- src/services/getScreenshotService.ts
+++ src/services/getScreenshotService.ts
@@ -49,23 +49,33 @@ export class GetScreenshotService {
         ])
       );
     }, newToken);
+    const openInitialTime = new Date().getTime();
     await page.goto(url, { waitUntil: "networkidle2" });
+    const openFinalTime = new Date().getTime();
+    const openTime = Math.round((openFinalTime - openInitialTime) * 1000);
+    console.log("Page is Rendered in " + openTime);
+    
     const filepath = path.resolve(__dirname, "..", "files");
     const filename = `file_${Math.round(Math.random() * 10000000000099)}.png`;
+    const screenshotInitialTime = new Date().getTime();
+    console.log('Initializing screenshot page browser');
     await page.screenshot({
       path: path.join(filepath, filename),
       clip: { height: 720, width: 1280, x: 0, y: 0 },
       quality: 100 * multiplier,
       type: "webp",
     });
-    // const file = fs.readFileSync(filename);
+    const screenshotFinalTime = new Date().getTime();
+    const screenshotTime = Math.round((screenshotFinalTime - screenshotInitialTime) * 1000);
+    console.log("Screenshot taked in " + screenshotTime);
+    console.log("The stream total time: " + screenshotTime + openTime);
+    
     res.setHeader("Content-Type", "image/webp");
     const stream = fs.createReadStream(path.join(filepath, filename));
     stream.pipe(res);
-    await page.close();
-
-    setTimeout(() => {}, 4500);
-    fs.unlinkSync(path.join(filepath, filename));
-    // return res.status(200).json({ error: false, message: "Deu bom" });
+    page.close().then(() => {
+      setTimeout(() => {}, 4500);
+      fs.unlinkSync(path.join(filepath, filename));
+    });
   }
 }
